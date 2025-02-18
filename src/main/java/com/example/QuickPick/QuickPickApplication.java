19d8dc060a7976be4773900765a278a0de5f874f
Vraj Modi <modivraj1975@gmail.com>
2025-02-18 20:02:49 +0530

    Initial Commit

---
+++ src/main/java/com/example/QuickPick/QuickPickApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuickPick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickPickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickPickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QuickPick/QuickPickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuickPick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickPickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
