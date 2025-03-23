852a5c470a2b88cf17cac55e46ab485cfb0ac11f
Vraj Modi <modivraj1975@gmail.com>
2025-03-23 23:39:48 +0530

    Added CategoryPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import CategoryPage from "./components/CategoryPage";
+import CategoryPage from "./components/CategoryPage";
 // import ItemListingPage from "./components/ItemListingPage";
 // import ProductPage from "./components/ProductPage";
 // import NotFoundPage from "./components/NotFoundPage";
@@ -12,8 +12,8 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/categories" element={<CategoryPage />} />
-        <Route path="/categories/:category" element={<ItemListingPage />} />
+         <Route path="/categories" element={<CategoryPage />} />
+        {/*<Route path="/categories/:category" element={<ItemListingPage />} />
         <Route path="categories/:category/:product" element={<ProductPage />} />
         <Route path="categories/:category/:product/details" element={<ProductDetailsPage />} />
         <Route path="*" element={<NotFoundPage />} /> */}
